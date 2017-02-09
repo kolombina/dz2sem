d78b6458c2e761db4208ba9e10afd623afe34d27
Сулейманова Даша <kolombina0711@gmail.com>
2017-02-09 18:59:20 +0300

    Update angem.py

--- angem.py
+++ angem.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from math import *
+import math
 
 n = 0
 a = []
@@ -32,8 +32,7 @@ with open("input.txt", "rt") as file1:
         if i > T-1:
             f = False
             break
-    #for i in range(0, T):
-    #    print (a[i][0], ' ', a[i][1])
+
 
     i = int(0)  # координаты окружностей
     j = int(0)
@@ -50,17 +49,29 @@ with open("input.txt", "rt") as file1:
             f = False
             break
 
+max = int(0)
+per = int(0)
+for i in range (0, T-1):
+    for j in range (i+1, T):
+        per = int(0)
+        Ax = (a[j][1])-(a[i][1])
+        By = -((a[j][0])-(a[i][0]))
+        C = (a[i][1])*((a[j][0])-(a[i][0]))-((a[j][1])-(a[i][1]))*(a[i][0])
+        for k in range (0,O):
+            d = (math.fabs(Ax * (b[k][0]) + By * (b[k][1]) + C)) / (math.sqrt(Ax * Ax + By * By))
+            if (d < (b[k][2])) or (d == (b[k][2])):
+                per+=1
+        if per > max:
+            max = per
+            x1 = int(a[i][0])
+            x2 = int(a[j][0])
+            y1 = int(a[i][1])
+            y2 = int(a[j][1])
+
+print (max)
+print (x1, ' ', y1)
+print (x2, ' ', y2)
 
 
 
 
-
-
- #  Ax       By              C
-#(y2-y1)*x | -y*(x2-x1) | +y1*(x2-x1)-(y2-y1)*x1 = 0 #уравнение прямой
-
-#  первые два числа в инпуте - координаты центра окружности - Mx и My
-#  math.fabs - модуль
-#  math.sqrt - корень
-#  d = (math.fabs (A*Mx+B*My+C))/(math.sqrt(A*A+B*B))
-#  если d < R, то инкреминируем
